d4da1a1634fa07d1eeb35cb882ec45dc1405399a
Vanessa Ilombe <vnsssitailombe@googlemail.com>
2024-03-01 21:09:47 +0000

    deployment setting

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,9 +11,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -71,17 +80,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'codestar.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',       
+#  }
+#}
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
